85a9756a076abdf0b25ed72cfbad5ebb4483b181
LarrySonar <larry.weidig@sonar.software>
2024-08-29 18:54:53 +0000

    Use optimize over separate commands

--- docker/php/app_init.sh
+++ docker/php/app_init.sh
@@ -5,7 +5,6 @@ cd /var/www/html
 composer update nothing
 
 rm -f bootstrap/cache/*
-php artisan optimize:clear
 
 php artisan key:generate
 
@@ -13,6 +12,7 @@ php artisan make:session-table -q
 php artisan migrate --force
 php artisan migrate:fresh
 
+php artisan optimize:clear
 php artisan optimize
 
 php artisan sonar:netflow:initialize
