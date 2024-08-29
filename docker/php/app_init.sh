d643e805026cdd31f0e128ff7952588021ef4e51
LarrySonar <larry.weidig@sonar.software>
2024-08-29 17:40:21 +0000

    Allow laravel to generate app key so format is acceptable.

--- docker/php/app_init.sh
+++ docker/php/app_init.sh
@@ -1,14 +1,19 @@
 #!/bin/ash
 
 cd /var/www/html
-composer update nothing
-rm -f bootstrap/cache/*
-php artisan cache:clear
-php artisan view:clear
 
+composer update nothing
+
+rm -f bootstrap/cache/*
 php artisan make:session-table -q
 php artisan migrate --force
 php artisan migrate:fresh
+
+php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+
+php artisan key:generate
 
 php artisan route:cache
 php artisan config:cache
